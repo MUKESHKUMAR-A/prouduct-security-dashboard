ab39c24e4996d666ca81c4b93700afa8099a4abe
MUKESHKUMAR A <103376155+MUKESHKUMAR-A@users.noreply.github.com>
2025-07-18 00:42:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
-function loadVAPTChart(filter = "All") {
-fetch('data/vapt.json')
-  .then(res => res.json())
-  .then(data => {
-    const labels = data.map(item => item.product);
-    const issues = data.map(item => item.critical_issues);
 
-    const ctx = document.getElementById('vaptChart').getContext('2d');
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Critical Issues (VAPT)',
-          data: issues,
-          backgroundColor: '#007bff',
-        }]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: 'VAPT Critical Issues by Product'
-          }
-        },
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  });
-}
 function loadSASTChart(filter = "All") {
   fetch('data/sast.json')
     .then(res => res.json())
@@ -75,8 +41,6 @@ function loadSASTChart(filter = "All") {
 }
 
 loadSASTChart();
-loadVAPTChart();
-
 document.getElementById('productFilter').addEventListener('change', (e) => {
   document.getElementById('sastChart').remove(); // Remove old canvas
   const newCanvas = document.createElement('canvas');
@@ -84,6 +48,49 @@ document.getElementById('productFilter').addEventListener('change', (e) => {
   document.body.appendChild(newCanvas);
   loadSASTChart(e.target.value);
 });
+
+function loadVAPTChart(filter = "All") {
+  fetch('data/vapt.json')
+    .then(res => res.json())
+    .then(data => {
+      if (filter !== "All") {
+        data = data.filter(item => item.product === filter);
+      }
+
+      const labels = data.map(item => item.product);
+      const issues = data.map(item => item.open_issues);
+
+      const ctx = document.getElementById('vaptChart').getContext('2d');
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Open Issues',
+            data: issues,
+            backgroundColor: '#007bff',
+          }]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              text: 'VAPT Open Issue Count by Product'
+            }
+            
+          },
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    });
+}
+
+loadVAPTChart();
 document.getElementById('productFilter').addEventListener('change', (e) => {
   document.getElementById('vaptChart').remove(); // Remove old canvas
   const newCanvas = document.createElement('canvas');

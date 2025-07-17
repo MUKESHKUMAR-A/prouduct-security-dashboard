ec3e80329ec0708821ecd7f6024268ddc823cbb5
MUKESHKUMAR A <103376155+MUKESHKUMAR-A@users.noreply.github.com>
2025-07-18 00:18:50 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+fetch('data/sast.json')
+  .then(res => res.json())
+  .then(data => {
+    const labels = data.map(item => item.product);
+    const issues = data.map(item => item.open_issues);
+
+    const ctx = document.getElementById('sastChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Open Issues',
+          data: issues,
+          backgroundColor: '#f44336',
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'SAST Open Issue Count by Product'
+          }
+        },
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
 
